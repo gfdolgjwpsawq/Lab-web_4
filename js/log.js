1379cb761d9a51c920a76617c3c01bd2984a818d
gfdolgjwpsawq <146908599+gfdolgjwpsawq@users.noreply.github.com>
2023-11-15 09:36:11 +0500

    Add files via upload

---
+++ js/log.js
@@ -0,0 +1,10 @@
+/*let button = document.getElementById("togglePassword");
+
+button.addEventListener("change", function() {
+    let pass_input = document.getElementsByClassName("password");
+    if (button.cheked) {
+        pass_input.type = "text";
+    } else {
+        pass_input.type = "password";
+    }
+});*/
